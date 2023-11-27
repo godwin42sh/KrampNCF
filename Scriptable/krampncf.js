73ec5e525342df20685d97ca63372bffea264010
Jacques Kramp <jacques1.kramp@epitech.eu>
2023-11-27 10:30:17 +0100

    feat: update sciptable script

--- Scriptable/krampncf.js
+++ Scriptable/krampncf.js
@@ -1,30 +1,48 @@
 const widget = new ListWidget();
-
-const token = "";
-const req = new Request("https://"+token+"@sncf.krampflix.ovh/departures");
+const token = "6s8Xd8ybPfue65nMLHs:6s8Xd8ybPfue65nMLHs";
+const req = new Request("https://sncf.krampflix.ovh/departures");
 const reqData = await req.loadJSON();
-const now = Date.now();
-widget.refreshAfterDate = new Date(now + 1000 * 60 * 2); // we refresh every 2 minutes
+
+const dateNow = new Date();
+const hours = dateNow.getHours();
+
+if ((hours >= 7 && hours <= 8) || (hours >= 16 && hours <= 18)) {
+    widget.refreshAfterDate = new Date(dateNow.getTime() + 1000 * 60 * 3);
+}
+else {
+    widget.refreshAfterDate = new Date(dateNow.getTime() + 1000 * 60 * 30);
+}
 
 const stack = widget.addStack();
 stack.layoutHorizontally();
+stack.topAlignContent();
 
 function makeTrain(train) {
   const trainStack = stack.addStack();
   trainStack.layoutVertically();
+
   const title = trainStack.addText(train.title);
+
   title.font = Font.boldSystemFont(16);
+  trainStack.addSpacer(5);
 
   if (train.data.length === 0) {
     trainStack.addText("Pas de train");
     return;
   }
-
   train.data.forEach(data => {
-    trainStack.addText(data.title);
-    trainStack.addText(data.departureTime);
-    trainStack.addText(data.arrivaleTime);
+    const dataStack = trainStack.addStack();
+    dataStack.layoutHorizontally();
+
+    const nameStation = dataStack.addText(data.title);
+    nameStation.font = Font.systemFont(14);
+    dataStack.addSpacer(5);
+
+    const time = dataStack.addText(data.departureTime);
+    time.font = Font.boldSystemFont(16);
   });
+
+    trainStack.setPadding(0, 10, 0, 10);
 }
 
 reqData.forEach(train => {
