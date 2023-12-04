8c13d7348a7eed38a4004b5c8ce50c1ac8d413ed
Jacques Kramp <jacques1.kramp@epitech.eu>
2023-12-04 18:45:30 +0100

    update Scriptable scripts

--- Scriptable/krampncf-rt.js
+++ Scriptable/krampncf-rt.js
@@ -1,3 +1,6 @@
+// Variables used by Scriptable.
+// These must be at the very top of the file. Do not edit.
+// icon-color: deep-purple; icon-glyph: magic;
 const widget = new ListWidget();
 const token = "6s8Xd8ybPfue65nMLHs:6s8Xd8ybPfue65nMLHs";
 
@@ -6,10 +9,10 @@ const hours = dateNow.getHours();
 const idDeparture = args.widgetParameter;
 
 if ((hours >= 7 && hours <= 8) || (hours >= 16 && hours <= 18)) {
-    widget.refreshAfterDate = new Date(dateNow.getTime() + 2000 * 60 * 3);
+    widget.refreshAfterDate = new Date(dateNow.getTime() + 1000 * 60 * 3);
 }
 else {
-    widget.refreshAfterDate = new Date(dateNow.getTime() + 2000 * 60 * 30);
+    widget.refreshAfterDate = new Date(dateNow.getTime() + 1000 * 60 * 15);
 }
 
 const stack = widget.addStack();
@@ -41,22 +44,36 @@ function makeTrain(train) {
     nameStation.font = Font.systemFont(14);
     dataStack.addSpacer(5);
 
-    let timeText = data.time;
-    let color = Color.White;
+    let timeText = data.departure.time;
+    let color = Color.white();
 
     if (data.departure.delay) {
-      timeText = `${data.time} + ${data.departure.delay}m`;
+      timeText = `${data.departure.time} +${data.departure.delay}m`;
+      color = Color.red();
     }
     const time = dataStack.addText(timeText);
     time.font = Font.boldSystemFont(16);
+    time.textColor = color;
   });
 
   trainStack.setPadding(0, 10, 0, 10);
 }
 
+function setTransparentBackground(widget) {
+  const fileName = "Transparent-m-t.jpg"
+  const files = FileManager.local()
+  const filePath = files.joinPath(files.documentsDirectory(), fileName)
+
+  if (files.fileExists(filePath)) {
+    widget.backgroundImage = files.readImage(filePath)
+  }
+}
+
 const url = `https://${token}@sncf.krampflix.ovh/departuresRT/${(idDeparture ? idDeparture : "")}`;
 const req = new Request(url);
 const reqData = await req.loadJSON();
+
+console.log(reqData);
 
 if (Array.isArray(reqData)) {
   reqData.forEach(train => {
@@ -66,6 +83,9 @@ if (Array.isArray(reqData)) {
 else {
   makeTrain(reqData);
 }
+
+widget.addSpacer()
+setTransparentBackground(widget)
 
 Script.setWidget(widget);
 Script.complete();
--- Scriptable/krampncf.js
+++ Scriptable/krampncf.js
@@ -1,15 +1,19 @@
+// Variables used by Scriptable.
+// These must be at the very top of the file. Do not edit.
+// icon-color: red; icon-glyph: magic;
 const widget = new ListWidget();
 const token = "6s8Xd8ybPfue65nMLHs:6s8Xd8ybPfue65nMLHs";
 
 const dateNow = new Date();
 const hours = dateNow.getHours();
-const idDeparture = args.widgetParameter;
+const onlyDelays = args.widgetParameter === "delays";
+const idDeparture = args.widgetParameter !== "delays" ? args.widgetParameter : false;
 
 if ((hours >= 7 && hours <= 8) || (hours >= 16 && hours <= 18)) {
-    widget.refreshAfterDate = new Date(dateNow.getTime() + 1000 * 60 * 3);
+	widget.refreshAfterDate = new Date(dateNow.getTime() + 1000 * 60 * 3);
 }
 else {
-    widget.refreshAfterDate = new Date(dateNow.getTime() + 1000 * 60 * 30);
+	widget.refreshAfterDate = new Date(dateNow.getTime() + 1000 * 60 * 30);
 }
 
 const stack = widget.addStack();
@@ -21,10 +25,10 @@ function makeTrain(train) {
   trainStack.layoutVertically();
 
   const title = trainStack.addText(train.title);
-
+  
   title.font = Font.boldSystemFont(16);
   trainStack.addSpacer(5);
-
+  
   if (train.data.length === 0) {
     trainStack.addText("Pas de train");
     return;
@@ -33,29 +37,56 @@ function makeTrain(train) {
     const dataStack = trainStack.addStack();
     dataStack.layoutHorizontally();
 
-    const nameStation = dataStack.addText(data.title);
-    nameStation.font = Font.systemFont(14);
-    dataStack.addSpacer(5);
+    if (!onlyDelays) {
+      const nameStation = dataStack.addText(data.title);
+      nameStation.font = Font.systemFont(14);
+      dataStack.addSpacer(5);
+    }
 
-    const time = dataStack.addText(data.departureTime);
+    let timeText = data.departureTime;
+    let color = Color.white();
+
+    if (data.delay) {
+      timeText = `${data.departureTime} +${data.delay}m`;
+      color = Color.red();
+    }
+
+    const time = dataStack.addText(timeText);
     time.font = Font.boldSystemFont(16);
+    time.textColor = color;
   });
-
-    trainStack.setPadding(0, 10, 0, 10);
+  
+	trainStack.setPadding(0, 10, 0, 10);
 }
 
-const url = `https://${token}@sncf.krampflix.ovh/departures/${(idDeparture ? idDeparture : "")}`;
+function setTransparentBackground(widget) {
+  const fileName = "Transparent-m-m.jpg"
+  const files = FileManager.local()
+  const filePath = files.joinPath(files.documentsDirectory(), fileName)
+
+  if (files.fileExists(filePath)) {
+    widget.backgroundImage = files.readImage(filePath)
+  }
+}
+
+const url = "https://"+token+"@sncf.krampflix.ovh/departures/" + (idDeparture ? idDeparture : "");
 const req = new Request(url);
 const reqData = await req.loadJSON();
 
 if (Array.isArray(reqData)) {
   reqData.forEach(train => {
+    if (onlyDelays && train.data.every(data => !data.delay)) {
+      return;
+    }
     makeTrain(train);
   });
 }
 else {
   makeTrain(reqData);
 }
+
+widget.addSpacer()
+setTransparentBackground(widget)
 
 Script.setWidget(widget);
 Script.complete();
