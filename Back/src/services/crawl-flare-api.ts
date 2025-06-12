708c44173bd2b74e21cfde147a03d2802107843a
Jacques Kramp <jacques@tekker.io>
2025-06-12 21:05:00 +0200

    fix flaresolverr problem+ migrate to bun + update packages

--- Back/src/services/crawl-flare-api.ts
+++ Back/src/services/crawl-flare-api.ts
@@ -6,77 +6,73 @@ import { CrawlFlareDeparture } from "src/types/CrawlFlareDeparture";
 import { CrawlData } from "src/types/CrawlData";
 
 type BodyFlare = {
-  cmd: string,
-  url: string,
-  maxTimeout: number
+  cmd: string;
+  url: string;
+  maxTimeout: number;
 };
 
-type DataType = 'Departures' | 'Arrivals';
+type DataType = "Departures" | "Arrivals";
 
 export class CrawlFlare {
-
   private api: AxiosInstance;
 
   private sncfUrl: string;
 
   private regexMatchJson = /(?:.*)(\[\{.*])(?:<\/pre>.*)/;
 
-  constructor(flaresolverrUrl: string, apiUrl: string, dataType: DataType = 'Departures') {
+  constructor(
+    flaresolverrUrl: string,
+    apiUrl: string,
+    dataType: DataType = "Departures",
+  ) {
     this.sncfUrl = `${apiUrl}/${dataType}`;
     this.api = axios.create({
       baseURL: flaresolverrUrl,
       headers: {
-        "Content-Type": "application/json"
-      }
+        "Content-Type": "application/json",
+      },
     });
-  }
-
-  private getJsonFromHtml(html: string): CrawlFlareDeparture[] {
-    const match = html.match(this.regexMatchJson);
-    if (match) {
-      const parsed = JSON.parse(match[1]) as CrawlFlareDeparture[];
-      return parsed;
-    }
-    return [];
   }
 
   private makeBody(stationId: string): BodyFlare {
     return {
       cmd: "request.get",
       url: `${this.sncfUrl}/${stationId}`,
-      maxTimeout: 60000
+      maxTimeout: 60000,
     };
   }
 
-  async getDepartures(crawlData: CrawlData): Promise<IsCached<CrawlFlareDeparture[]>> {
-
-    const redis = new Redis((process.env.REDIS_URL as string));
+  async getDepartures(
+    crawlData: CrawlData,
+  ): Promise<IsCached<CrawlFlareDeparture[]>> {
+    const redis = new Redis(process.env.REDIS_URL as string);
 
     const body = this.makeBody(crawlData.flareId);
     const redisKey = body.url;
 
     const cached = await redis.get(redisKey);
-    const cacheTime = process.env.REDIS_CRAWL_EXPIRE ? parseInt(process.env.REDIS_CRAWL_EXPIRE) : 300;
+    const cacheTime = process.env.REDIS_CRAWL_EXPIRE
+      ? parseInt(process.env.REDIS_CRAWL_EXPIRE)
+      : 300;
 
     if (cached) {
       return {
         isCached: true,
         data: JSON.parse(cached),
       };
-    }
-    else {
+    } else {
       let resData: CrawlFlareDeparture[] = [];
-      console.log('crawling from SNCF site with flaresolverr');
+      console.log("crawling from SNCF site with flaresolverr");
 
       try {
-        const { data } = await this.api.post('', body);
+        const { data } = await this.api.post("", body);
 
-        resData = this.getJsonFromHtml(data.solution.response);
-        redis.set(redisKey, JSON.stringify(resData), 'EX', cacheTime);
-      }
-      catch (e) {
+        const resData = data.solution.response;
+        redis.set(redisKey, JSON.stringify(resData), "EX", cacheTime);
+      } catch (e) {
+        console.log("error", e);
         if (axios.isAxiosError(e)) {
-          console.log('error while crawling', e.message);
+          console.log("error while crawling", e.message);
         }
       }
 
